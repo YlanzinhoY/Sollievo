23d32787019905fefd7d1014e17c748778dc981f
Enzo Lanzellotti <102574758+YlanzinhoY@users.noreply.github.com>
2024-07-11 14:46:34 -0300

    refatorando o toolingController, colocando uma interface nele e mais uma struct para bindar, vou ter que refatorar mas por enquanto ta suave

--- controller/toolingController.go
+++ controller/toolingController.go
@@ -8,7 +8,21 @@ import (
 	"net/http"
 )
 
-func ToolingController() {
+type ToolsServiceInterface interface {
+	Exec(commandsStruct *service.CommandsStruct, value, goPackage string)
+}
+
+type ToolingControllerUpper struct{}
+
+func (t *ToolingControllerUpper) Exec(commandsStruct *service.CommandsStruct, value, goPackage string) {
+	err := commandsStruct.CommandRunner(value, goPackage)
+	if err != nil {
+		fmt.Println(http.StatusBadRequest)
+	}
+	fmt.Printf("%d\n", http.StatusOK)
+}
+
+func (t *ToolingControllerUpper) ToolingController() {
 	toolsChoices := make([]string, 0, 3)
 	commandsStruct := service.CommandsStruct{}
 	modelTools := model.Tools{}
@@ -19,27 +33,12 @@ func ToolingController() {
 	//region switch case
 	for _, choice := range tools {
 		switch choice {
-		case enums.Gorm:
-			err := commandsStruct.CommandRunner(enums.Gorm, enums.GormPackage)
-			if err != nil {
-				fmt.Println(http.StatusBadRequest)
-				break
-			}
-			fmt.Printf("%d\n", http.StatusOK)
-		case enums.Viper:
-			err := commandsStruct.CommandRunner(enums.Viper, enums.ViperPackage)
-			if err != nil {
-				fmt.Println(http.StatusBadRequest)
-				break
-			}
-			fmt.Printf("%d\n", http.StatusOK)
-		case enums.Wire:
-			err := commandsStruct.CommandRunner(enums.Wire, enums.WirePackage)
-			if err != nil {
-				fmt.Println(http.StatusBadRequest)
-				break
-			}
-			fmt.Printf("%d\n", http.StatusOK)
+		case toolsChoices[0]:
+			t.Exec(&commandsStruct, enums.Gorm, enums.GormPackage)
+		case toolsChoices[1]:
+			t.Exec(&commandsStruct, enums.Viper, enums.ViperPackage)
+		case toolsChoices[2]:
+			t.Exec(&commandsStruct, enums.Wire, enums.WirePackage)
 		case enums.Default:
 			fmt.Println("Saindo...")
 			break
