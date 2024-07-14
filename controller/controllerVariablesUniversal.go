40383885fc51d05b4f9d39903a7c569d31726d9d
Enzo Lanzellotti <102574758+YlanzinhoY@users.noreply.github.com>
2024-07-14 03:19:18 -0300

    refatorando um pouco do codigo

--- controller/controllerVariablesUniversal.go
+++ controller/controllerVariablesUniversal.go
@@ -1,9 +1,42 @@
 package controller
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.ylanzinhoy.tooling_golang/enums"
 	"github.ylanzinhoy.tooling_golang/model"
 	"github.ylanzinhoy.tooling_golang/service"
 )
 
 var commandsStruct = service.CommandsStruct{}
 var modelTools = model.Tools{}
+
+type ToolsServiceInterface interface {
+	Exec(commandsStruct *service.CommandsStruct, value, goPackage string)
+	Back(commandsStruct *service.CommandsStruct, command string)
+	Exit(commandsStruct *service.CommandsStruct)
+}
+
+type ToolingControllerUpper struct{}
+
+func (t *ToolingControllerUpper) Exec(commandsStruct *service.CommandsStruct, value, goPackage string) {
+	err := commandsStruct.CommandRunner(value, goPackage)
+	if err != nil {
+		fmt.Println(http.StatusBadRequest)
+	}
+	fmt.Printf("%d\n", http.StatusOK)
+}
+
+func (t *ToolingControllerUpper) Back(commandsStruct *service.CommandsStruct, command string) {
+	err := commandsStruct.Back(command)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+}
+
+func (s *ToolingControllerUpper) Exit(commandsStruct *service.CommandsStruct) {
+	fmt.Println(enums.Purple + "Saindo...")
+}
--- controller/toolingController.go
+++ controller/toolingController.go
@@ -1,41 +1,9 @@
 package controller
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.ylanzinhoy.tooling_golang/enums"
-	"github.ylanzinhoy.tooling_golang/service"
 )
 
-type ToolsServiceInterface interface {
-	Exec(commandsStruct *service.CommandsStruct, value, goPackage string)
-	Back(commandsStruct *service.CommandsStruct, command string)
-	Exit(commandsStruct *service.CommandsStruct)
-}
-
-type ToolingControllerUpper struct{}
-
-func (t *ToolingControllerUpper) Exec(commandsStruct *service.CommandsStruct, value, goPackage string) {
-	err := commandsStruct.CommandRunner(value, goPackage)
-	if err != nil {
-		fmt.Println(http.StatusBadRequest)
-	}
-	fmt.Printf("%d\n", http.StatusOK)
-}
-
-func (t *ToolingControllerUpper) Back(commandsStruct *service.CommandsStruct, command string) {
-	err := commandsStruct.Back(command)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-}
-
-func (s *ToolingControllerUpper) Exit(commandsStruct *service.CommandsStruct) {
-	fmt.Println(enums.Purple + "Saindo...")
-}
 func (t *ToolingControllerUpper) ToolingController() {
 	toolsChoices := make([]string, 0)
 
