506dae4e4bd1a22f4fcbc25469167a1a7205e56e
Enzo Lanzellotti <enzo.maxpayne@gmail.com>
2024-08-05 11:45:38 -0300

    criando uma struct para passar o path que precisa para criar os arquivos

--- internal/service/creatingFIlesBackEnd.go
+++ internal/service/creatingFIlesBackEnd.go
@@ -6,14 +6,22 @@ import (
 	"os"
 )
 
-func creatingFilesBackEnd(path string) {
+type CreatingFilesBackEnd struct {
+	path string
+}
 
-	dir := fmt.Sprintf("%s/backend", path)
+func (c *CreatingFilesBackEnd) creatingFilesBackEnd() {
+
+	dir := fmt.Sprintf("%s/backend", c.path)
 	err := os.MkdirAll(dir, os.ModePerm)
 
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+
+}
+
+func (c *CreatingFilesBackEnd) creatingStructureBase() {
 
 }
